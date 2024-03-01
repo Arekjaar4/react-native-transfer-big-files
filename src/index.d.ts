47c297b41e8cba508e67639eb061a991b13e75b3
Arekjaar <47115428+Arekjaar4@users.noreply.github.com>
2024-03-01 12:52:30 +0100

    first commit

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,8 @@
 import type { EmitterSubscription } from 'react-native'
 
-export const subscribeOnProgressUpdates: (callback: (progress: string) => void) => EmitterSubscription
+export let subscribeOnProgressUpdates: (callback: (progress: string) => void) => EmitterSubscription
 
-export const subscribeOnFileCopyEnd: (callback: (data: string) => void) => EmitterSubscription
+export let subscribeOnFileCopyEnd: (callback: (data: string) => void) => EmitterSubscription
 
 export const sendFileTo: (pathToFile: string, address: string) => Promise<{ time: number, file: string }>
 export const receiveFile: (ip: string, folder: string, fileName: string, forceToScanGallery?: boolean) => Promise<string>
@@ -11,5 +11,5 @@ export const receiveMessage: (ip: string, props: { meta: boolean }) => Promise<s
 export const stopReceivingMessage: () => void
 
 // system methods
-export const subscribeOnEvent: (event: string, callback: Function) => EmitterSubscription
+export let subscribeOnEvent: (event: string, callback: Function) => EmitterSubscription
 
