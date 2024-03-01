2d66b5b89c0ead998206a6fe1a7b424a87c1b8b1
Arekjaar <47115428+Arekjaar4@users.noreply.github.com>
2024-03-01 12:55:32 +0100

    first commit

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,11 +4,11 @@ export let subscribeOnProgressUpdates: (callback: (progress: string) => void) =>
 
 export let subscribeOnFileCopyEnd: (callback: (data: string) => void) => EmitterSubscription
 
-export const sendFileTo: (pathToFile: string, address: string) => Promise<{ time: number, file: string }>
-export const receiveFile: (ip: string, folder: string, fileName: string, forceToScanGallery?: boolean) => Promise<string>
-export const sendMessageTo: (message: string, address: string) => Promise<{ time: number, message: string }>
-export const receiveMessage: (ip: string, props: { meta: boolean }) => Promise<string>
-export const stopReceivingMessage: () => void
+export let sendFileTo: (pathToFile: string, address: string) => Promise<{ time: number, file: string }>
+export let receiveFile: (ip: string, folder: string, fileName: string, forceToScanGallery?: boolean) => Promise<string>
+export let sendMessageTo: (message: string, address: string) => Promise<{ time: number, message: string }>
+export let receiveMessage: (ip: string, props: { meta: boolean }) => Promise<string>
+export let stopReceivingMessage: () => void
 
 // system methods
 export let subscribeOnEvent: (event: string, callback: Function) => EmitterSubscription
